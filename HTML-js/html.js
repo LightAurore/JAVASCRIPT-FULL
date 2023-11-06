3c40ab55b9238d1cab86881ca2d56275dad57e7b
LightAurore <115423063+LightAurore@users.noreply.github.com>
2023-11-06 15:56:30 +0100

    exo tableau

---
+++ HTML-js/html.js
@@ -0,0 +1,15 @@
+const maNav = document.getElementById("home-js");
+
+
+maNav.innerHTML = `
+<div class="home">
+    <a href="../../index.html">Home</a>
+</div>
+<nav class="">
+    <a href="./HTML-js/exo01-cv-picsou/cv.html"></a>
+    <a href="./HTML-js/exo02-cv-picsou-css/cv.html"></a>
+    <a href="./HTML-js/exo03-cv-contact/cv.html"></a>
+    <a href=""></a>
+</nav>
+
+`
