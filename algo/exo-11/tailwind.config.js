331e1ffa2b2966ac73d044fe2778d55a9697a973
LightAurore <115423063+LightAurore@users.noreply.github.com>
2023-11-20 11:59:42 +0100

    parcel

--- algo/exo-11/tailwind.config.js
+++ algo/exo-11/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        auto: 'auto',
+      }
+    },
   },
   plugins: [],
 }
