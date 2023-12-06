298a1e9e419b1cf00457ab9c3328c299e0065787
LightAurore <115423063+LightAurore@users.noreply.github.com>
2023-12-06 21:55:06 +0100

    objets conjugaison

--- fonctionnaliter/js/conjugueur.js
+++ fonctionnaliter/js/conjugueur.js
@@ -1,25 +1,177 @@
+const sujets = ["Na", "O", "A", "E", "To", "Bo", "Ba"];
 
-const sujets = ["Na", "O", "A", "E", "To", "Bo","Ba"];
+const conjugaison = [
+  {
+    mode: "indicatif",
+    temps: [
+      "présent",
+      "passé composé",
+      "imparfait",
+      "plus-que-parfait",
+      "futur simple",
+      "futur antérieure",
+      "passé simple",
+      "passé antérieur",
+    ],
+    terminaison: [
+      {
+        present: "zala",
+      },
+      {
+        passer_composer: "zala",
+      },
+      {
+        imparfait: "zala",
+      },
+      {
+        plus_que_parfait: "zala",
+      },
+      {
+        futur_simple: "zala",
+      },
+      {
+        futur_anterieur: "zala",
+      },
+      {
+        passer_simple: "zala",
+      },
+      {
+        passer_anterieur: "zala",
+      },
+    ],
+  },
+  {
+    mode: "subjonctif",
+    temps: ["présent", "imparfait", "passé", "plus-que-parfait"],
+  },
+  {
+    mode: "conditionnel",
+    temps: ["présent", "passé"],
+  },
+  {
+    mode: "imperatif",
+    temps: ["présent", "passé"],
+  },
+  {
+    mode: "participe",
+    temps: ["présent", "passé"],
+  },
+  {
+    mode: "infinitif",
+    temps: ["présent", "passé"],
+  },
+  {
+    mode: "gerondif",
+    temps: ["présent", "passé", "futur"],
+  },
+];
 
-const modes = ["indicatif", "subjonctif","conditionnel","imperatif","participe","infinitif", "gerondif"];
+const mode_indicatif = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_indicatif = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
+const mode_subjonctif = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_subjonctif = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
+const mode_conditionnel = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_conditionnel = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
+const mode_imperatif = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_imperatif = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
+const mode_participe = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_participe = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
+const mode_infinitif = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_infinitif = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
+const mode_gérondif = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_gérondif = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
+const mode_interrogatif = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_interrogatif = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
+const mode_recirpoque = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
 
-const mode_recirpoque = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
-
-const mode_autres = ["Présent", "Passé composé", "Impératif","Plus-que-parfait", "Passé simple", "Passé antérieur","Futur simple", "Futur antérieur"];
-
+const mode_autres = [
+  "Présent",
+  "Passé composé",
+  "Impératif",
+  "Plus-que-parfait",
+  "Passé simple",
+  "Passé antérieur",
+  "Futur simple",
+  "Futur antérieur",
+];
