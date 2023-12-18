5a999b0df005b3b767b226cf997625b47a087d42
LightAurore <115423063+LightAurore@users.noreply.github.com>
2023-12-18 15:55:11 +0100

    challenge css et exo 10 pendu

--- JS-Pratiques/Exo_10_Fonctions/src/assets/js/jeudupendu.js
+++ JS-Pratiques/Exo_10_Fonctions/src/assets/js/jeudupendu.js
@@ -5,8 +5,16 @@ import { listeDeMots, draw } from "./db.js";
 // DOM
 let startToPlay = document.getElementById("jouer");
 let stopPlaying = document.getElementById("stop");
-let motContainer = document.getElementById("mot-a-trouver");
+let lettres_container = document.getElementById("lettres-du-mot");
 let lettreProposition = document.getElementById("lettre-proposition");
+
+// pendu
+const tete = document.getElementById('tete');
+const tronc = document.getElementById('tronc');
+const bras_gauche = document.getElementById('bras-gauche');
+const bras_droit = document.getElementById('bras-droit');
+const jambe_droite = document.getElementById('jambe-droite');
+const jambe_gauche = document.getElementById('jambe-gauche');
 
 // Mes constatnte
 const MAX = listeDeMots.length;
@@ -15,56 +23,142 @@ const MIN = 1;
 // Variables globales
 let aleaIndex;
 let mot_a_trouver;
+let nbr_erreur = 0;
+let array = new Array();
+let arrayProps = new Array();
+let motArray;
+let saveLetters = "";
 
-// 2) Jeu du pendu
-// * Attentes fonctionnalités :
-//  <img src="./exemplePendu.PNG">
-/*   Instructions :    */
+//  Jeu du pendu
+
+/*   Initialisation :    */
+/**
+ * @param
+ * 
+ */
+function initialisation(){
+// images du pendu, cachée
+tete.style.visibility = "hidden";
+tronc.style.visibility = "hidden";
+bras_gauche.style.visibility = "hidden";
+bras_droit.style.visibility = "hidden";
+jambe_droite.style.visibility = "hidden";
+jambe_gauche.style.visibility = "hidden";
+
+// Dom
+lettreProposition.disabled = false;
+  lettres_container.innerHTML = "";
+
+// var globales
+  mot_a_trouver = "";
+  nbr_erreur = 0;
+  array = [];
+  arrayProps = [];
+  saveLetters = "";
+  motArray = "";
+}
+
 
 //  Le programme selectionne un mot aléatoirement dans une liste de mots
 startToPlay.addEventListener("click", function () {
   console.clear();
-  motContainer.innerHTML = "";
+  initialisation()
+
+  // mot à trouver généré
   aleaIndex = Math.floor(Math.random() * (MAX - MIN)) + MIN;
   mot_a_trouver = listeDeMots[aleaIndex];
   console.log("ton mot est là : " + mot_a_trouver);
+
+  // case à remplir
   for (let i = 0; i < mot_a_trouver.length; i++) {
     const html_span = document.createElement("span");
     html_span.innerText = "_";
     // html_span.innerText = mot_a_trouver[i];
     html_span.classList.add("letter");
-    motContainer.append(html_span);
+    lettres_container.append(html_span);
   }
 });
 
+// Arret du jeu
 stopPlaying.addEventListener("click", () => {
-  startToPlay.disabled = true;
+  let stop = confirm("Voulez vous arrêter de jouer ?")
+  if(stop){
+    startToPlay.disabled = true;
+    lettreProposition.disabled = true;
+    lettres_container.innerHTML = '';
+  }
 });
+
 
 //  Grâce à un input et un button, le joueur peut proposer une lettre
-let array = new Array();
+
 lettreProposition.addEventListener("change", function () {
-  //   console.log(mot_a_trouver.includes(lettreProposition.value));
 
-  if (mot_a_trouver.includes(lettreProposition.value)) {
-    let result = mot_a_trouver.split("");
+  arrayProps.push(lettreProposition.value);
+  motArray = arrayProps.join('');
+
+  let proposition = mot_a_trouver.normalize("NFD").replace(/[\u0300-\u036f]/g, '');
+
+  if (proposition.includes(lettreProposition.value)) {
+    let result = proposition.split("");
     let i = 0;
-    result.forEach((char) => {
-      if (char == lettreProposition.value) {
-        // console.log(char);
-        // const nieme_letter = result.indexOf(char);
 
-        console.log(i);
-        const target = motContainer.querySelectorAll("span")[i];
+    if(!proposition.includes(lettreProposition.value)){
+       
+      result.forEach((char) => {
+      if (char == lettreProposition.value) {
+        const target = lettres_container.querySelectorAll("span")[i];
         target.innerText = mot_a_trouver[i];
         array[i] = mot_a_trouver[i];
-        console.log(result === array);
+        saveLetters = array.join('');
+        if(saveLetters == mot_a_trouver){
+          alert("mot trouver");
+          startToPlay.disabled = true;
+          lettreProposition.disabled = true;
+        }
       }
       i -= -1;
-    });
+      });
+    }else{
+      alert("lettre déjà présente");
+    }
+    
     console.log(result === mot_a_trouver);
-  }
+  }else{
+    if(!motArray.includes(lettreProposition.value)){
+      alert("lettre déjà proposée")
+    }else{
+      nbr_erreur -=- 1;
+    }
+    /* --------------------------------------- */
+    // pendu, ajouter les parties
+    if(nbr_erreur == 1){
+      tete.classList.add("partie-display")
+    }
+    else if(nbr_erreur == 2){
+      tronc.classList.add("partie-display")
+    }
+    else if(nbr_erreur == 3){
+      bras_gauche.classList.add("partie-display")
+    }
+    else if(nbr_erreur == 4){
+      bras_droit.classList.add("partie-display")
+    }
+    else if(nbr_erreur == 5){
+      jambe_gauche.classList.add("partie-display")
+    }
+    else if(nbr_erreur == 6){
+      jambe_droite.classList.add("partie-display")
+    }
 
+    /* nombre d'erreur */
+    if(nbr_erreur >= 6){
+      startToPlay.disabled = true;
+      lettreProposition.disabled = true;
+      alert("Pendu, vous avez perdu !");
+    }
+  }
+  console.log("mon array", arrayProps);
   lettreProposition.value = "";
 });
 //  Si la lettre est dans le mot, on la dévoile (mise à jour de l'affichage du mot)
