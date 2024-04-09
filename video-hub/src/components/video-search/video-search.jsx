66c9163735b9bb77b83776e037c29fab1f5b34bf
LightAurore <115423063+LightAurore@users.noreply.github.com>
2024-04-09 15:41:09 +0200

    fakeData

--- video-hub/src/components/video-search/video-search.jsx
+++ video-hub/src/components/video-search/video-search.jsx
@@ -1,13 +1,45 @@
 import axios from "axios";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 // import 'dotenv/config';
 
 
+const fakeData = [
+    {
+        channel: "AIREMPLOI Espace Orientation ",
+        title: "Julie, Ingénieure en Intelligence Artificielle",
+        vidRefId: "h6qReMbUegc"
+    },
+    {
+        channel: "Disney Channel Belgique",
+        title: "La Bande à Picsou et l'Atlantide I La Bande à Picsou I Disney Channel BE",
+        vidRefId: "taw2cr0qdoo"
+    },
+    {
+        channel: " Le Parisien",
+        title: "Voici le robot humanoïde le plus réaliste au monde",
+        vidRefId: "Oi-W1O17VZE"
+    },
+    {
+        channel: "France 3 Grand Est",
+        title: "Le petit robot Nao aide les enfants autistes",
+        vidRefId: "UFEzx9Q9P0s"
+    },
+    {
+        channel: "Ultimate Fact",
+        title: "20 Amazing Robot Animals That Will Blow Your Mind",
+        vidRefId: "C_tiur59Q2E"
+    },
+    {
+        channel: "The Verge",
+        title: "Facebook's Cambridge Analytica data scandal, explained",
+        vidRefId: "VDR8qGmyEQg"
+    }
+]
 
 
-
-const VideoSearch = () => {
-    const [video, setVideo] = useState([])
+const VideoSearch = ({onSearchVid}) => {
+    const [video, setVideo] = useState([...fakeData]);
+    const [titleVid, setTitleVid] = useState('');
 
     const listVideo = async() => {
         
@@ -26,6 +58,7 @@ const VideoSearch = () => {
                 const data = {
                     channel: vidData[key].snippet.channelTitle,
                     desc: vidData[key].snippet.description,
+                    title: vidData[key].snippet.title,
                     vidRefId: vidData[key].id.videoId
                 }
 
@@ -43,21 +76,36 @@ const VideoSearch = () => {
         })
     }
 
-    listVideo()
+    // listVideo()
 
+
+    useEffect(() =>{
+        console.log('----- titleVid -----');
+        console.log(titleVid);
+    },[titleVid])
+
+    const handleSubmit = useCallback(function(){
+
+        onSearchVid(video);
+
+        console.log('----- data -----');
+        console.log(video);
+
+    },[video]
+)
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
-                <input type="search" name="" id="" />
-                <button>search</button>
-            </div>
-
-            <div>
-                <input type="file" />
+                <input type="search" id={titleVid} value={titleVid} onChange={(e) => setTitleVid(e.target.value)} />
+                <button type="submit">Rechercher</button>
             </div>
         </form>
     )
+}
+
+VideoSearch.defaultProps = {
+    onSearchVid: () => {}
 }
 
 export default VideoSearch;
