791ee02e1386b4e2514185d2316654d05ec3848f
Castor_web <castor.dev@users.noreply.github.com>
2024-04-28 20:50:04 +0200

    initiation de mobayi actu front avec react

---
+++ Mobayi/ubangi/mobayi-actu/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Counter from './components/counter/counter'
+import ArticlePageDetail from './pages/article-page/article-page-detail'
+
+function App() {
+
+  return (
+    <>
+      <Counter />
+      <ArticlePageDetail />
+    </>
+  )
+}
+
+export default App
